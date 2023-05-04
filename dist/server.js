ee8f9ecaaeea40ab04475502d6a4a6334dde982e
Maximilian <scientificresearch309@gmail.com>
2023-05-04 12:06:43 +0200

    we don't have a right format of JSON file, therefore, we install express and adapt it to the server.ts

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,12 @@
-import { createServer } from 'http';
-import { mainContent } from './content.js';
+import { createServer } from "http";
+import fs from "fs";
 const port = 3005;
+const jobs = JSON.parse(fs.readFileSync("./src/data/jobs.json", "utf8"));
+console.log(jobs);
 createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text-plain' });
-    res.write(mainContent);
+    res.writeHead(200, { "Content-Type": "text-plain" });
+    //   res.write(mainContent);
+    res.write(JSON.stringify(jobs));
     res.end();
 }).listen(port);
 console.log(`listening on http://localhost:${port}`);
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
-import { createServer } from 'http';
-import { mainContent } from './content.js';
-
+import { createServer } from "http";
+import { mainContent } from "./content.js";
+import fs from "fs";
 const port = 3005;
 
+const jobs = JSON.parse(fs.readFileSync("./src/data/jobs.json", "utf8"));
+console.log(jobs);
+
 createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text-plain' });
-	res.write(mainContent);
-	res.end();
+  res.writeHead(200, { "Content-Type": "text-plain" });
+  //   res.write(mainContent);
+  res.write(JSON.stringify(jobs));
+  res.end();
 }).listen(port);
 
 console.log(`listening on http://localhost:${port}`);
